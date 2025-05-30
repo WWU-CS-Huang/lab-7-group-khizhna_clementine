18066bcb03f53b8f677ac81923aa2b51d0b8f9b0
penrosm <penrosm@cf405-06.cs.wwu.edu>
2025-05-30 14:38:39 -0700

    Huffman.java

---
+++ app/src/main/java/lab7/Huffman.java
@@ -0,0 +1,13 @@
+package lab7;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+
+public class Huffman {
+
+    public static void main(String[] args) {
+       //do you see  
+    }
+}
